95153488d276f4160e63fc194f3bac0fff6222f2
seo-sang-ho <rocter2049@naver.com>
2023-11-22 14:48:50 +0900

    feat : QuestionRepositoryTest

--- src/test/java/com/mysite/sbb/SbbApplicationTests.java
+++ src/test/java/com/mysite/sbb/SbbApplicationTests.java
@@ -1,15 +1,14 @@
 package com.mysite.sbb;
 
-import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
-import java.time.LocalDateTime;
 import java.util.List;
 import java.util.Optional;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.*;
 
 @SpringBootTest
 class SbbApplicationTests {
@@ -52,14 +51,38 @@ class SbbApplicationTests {
 
 	@Test
 	void findBySubject(){
-		Question question = this.questionRepository.findBySubject("sbb는 무엇인가요?");
-		org.junit.jupiter.api.Assertions.assertEquals(1,question.getId());
+		Question question = this.questionRepository.findBySubject("sbb가 무엇인가요?");
+		assertEquals(1,question.getId());
 	}
 
 	@Test
-	void findBySubjectAndContent(){
-		Question q = this.questionRepository.findBySubjectAndContent("sbb는 무엇인가요?", "sbb에 대해서 알고 싶습니다.");
-		org.junit.jupiter.api.Assertions.assertEquals(1,q.getId());
+	void findBySubjectAndContent() {
+		Question q = this.questionRepository.findBySubjectAndContent("sbb가 무엇인가요?", "sbb에 대해서 알고 싶습니다.");
+		assertEquals(1, q.getId());
+
+	}
+	@Test
+	void findBySubjectLike(){
+		List<Question> qList = this.questionRepository.findBySubjectLike("sbb%");
+		Question q = qList.get(0);
+		assertEquals("sbb가 무엇인가요?",q.getSubject());
+	}
+	@Test
+	void modifyQuestion(){
+		Optional<Question> oq = this.questionRepository.findById(1);
+		assertTrue(oq.isPresent());
+		Question q = oq.get();
+		q.setSubject("수정된 제목");
+		this.questionRepository.save(q);
+	}
+	@Test
+	void deleteQuestion(){
+		assertEquals(2,this.questionRepository.count());
+		Optional<Question> oq = this.questionRepository.findById(1);
+		assertTrue(oq.isPresent());
+		Question q = oq.get();
+		this.questionRepository.delete(q);
+		assertEquals(1,this.questionRepository.count());
 
 	}
 }
