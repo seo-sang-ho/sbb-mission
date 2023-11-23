a021f7b98eea0efa3f4e6f8a80767c60510290e4
seo-sang-ho <rocter2049@naver.com>
2023-11-23 16:15:19 +0900

    2-08

--- src/main/java/com/mysite/sbb/MainController.java
+++ src/main/java/com/mysite/sbb/MainController.java
@@ -5,10 +5,15 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
-@ResponseBody
 public class MainController {
     @GetMapping("/sbb")
+    @ResponseBody
     String main(){
         return "sbb에 오신걸 환영합니다.";
+    }
+
+    @GetMapping("/")
+    public String root(){
+        return "redirect:/question/list";
     }
 }
