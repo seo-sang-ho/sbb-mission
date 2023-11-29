6eb5dd30bd039edad38ae11cd73d1b74a06128e5
seo-sang-ho <rocter2049@naver.com>
2023-11-29 22:26:34 +0900

    3-11

--- src/main/java/com/mysite/sbb/question/Question.java
+++ src/main/java/com/mysite/sbb/question/Question.java
@@ -8,6 +8,7 @@ import lombok.Setter;
 
 import java.time.LocalDateTime;
 import java.util.List;
+import java.util.Set;
 
 @Getter
 @Setter
@@ -33,4 +34,6 @@ public class Question {
 
     private LocalDateTime modifyDate;
 
+    @ManyToMany
+    Set<SiteUser> voter;
 }
